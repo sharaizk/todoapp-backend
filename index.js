9f15be21cfd60c261a6f202d82412c6252888e46
sharaizk <64202206+sharaizk@users.noreply.github.com>
2021-07-17 15:20:38 +0500

    Body too large fix

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(fileupload({
 }))
 const PORT = process.env.PORT ||5000
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+
 app.use(cors())
 app.use(require('./routes/userRoutes'))
 app.use(require('./routes/taskRoutes'))
